83c1a45bc8dc69971ce39fd6f15e6170d0de46ab
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-07-27 16:02:48 +0530

    Create minelement.cpp

---
+++ array/minelement.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int SmallestElement(int arr[], int n) {
+ 
+  int min = arr[0];
+  for (int i = 0; i < n; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
