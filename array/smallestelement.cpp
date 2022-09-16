48f3c5716affd8c6d566e41154e53e327ecfcfe4
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-09-16 12:25:05 +0530

    Update smallestelement.cpp

--- array/smallestelement.cpp
+++ array/smallestelement.cpp
@@ -1,3 +1,4 @@
+// Using minimum variable and compaing with every other element
 #include <bits/stdc++.h>
  
 using namespace std;
@@ -10,4 +11,25 @@ int SmallestElement(int arr[], int n) {
     }
   }
   return min;
+}
+
+
+// using sorting with built in  stl
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int sortArr(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    return arr[0];
+}
+ 
+int main() {
+    vector<int> arr1 = {2,5,1,3,0};
+    vector<int> arr2 = {8,10,5,7,9};
+   
+    cout<<"The smallest element in the array is: "<<sortArr(arr1)<<endl;
+    cout<<"The smallest element in the array is: "<<sortArr(arr2);
+   
+    return 0;
 }
