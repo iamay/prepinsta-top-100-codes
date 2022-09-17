ea67e0338fe6bc8369db346bcdb015f795dbc3f4
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-09-17 13:56:49 +0530

    Update max-subarray-sum.cpp

--- array/max-subarray-sum.cpp
+++ array/max-subarray-sum.cpp
@@ -73,3 +73,18 @@ class Solution{
         return maxsum;        
     }
 };
+
+// OR
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+         int curMax = 0, maxTillNow = INT_MIN;
+        for(auto c : nums)
+            curMax = max(c, curMax + c),
+            maxTillNow = max(maxTillNow, curMax);
+        return maxTillNow;
+    }
+
+};
